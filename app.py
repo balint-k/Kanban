f435cd0fef9cd80263634d57f231caaa42f1abfa
balintkeri <balintkeri0515@gmail.com>
2025-09-11 08:50:09 +0200

    columns added

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, jsonify
-
+from models import Database
 
 app = Flask(__name__)
+db = Database()
 
 @app.route('/')
 def board():
-    return render_template('board.html')
+    columns = db.get_columns()
+    return render_template('board.html', columns=columns)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
