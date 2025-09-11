5368cc8cc69b4a60ac9fab66e2fd1960ea91551f
balintkeri <balintkeri0515@gmail.com>
2025-09-11 08:30:14 +0200

    base app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def board():
+    return render_template('board.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
