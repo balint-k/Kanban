7294c17862d2b98bc4842724a69a7232bf3152b9
balintkeri <balintkeri0515@gmail.com>
2025-09-12 08:18:34 +0200

    Task added to getData

--- models.py
+++ models.py
@@ -2,9 +2,17 @@ import sqlite3
 from dataclasses import dataclass
 
 @dataclass
+class Task:
+    id: int
+    title: str
+    description: str
+    column_id: int
+
+@dataclass
 class Column:
     id: int
     name: str
+    tasks: list
 
 
 class Database:
@@ -20,51 +28,66 @@ class Database:
                     name TEXT NOT NULL
                 )
             """)
-            #self.conn.execute("""
-            #    CREATE TABLE IF NOT EXISTS tasks (
-            #        id INTEGER PRIMARY KEY AUTOINCREMENT,
-            #        title TEXT NOT NULL,
-            #        description TEXT,
-            #        column_id INTEGER,
-            #        FOREIGN KEY(column_id) REFERENCES columns(id)
-            #    )
-            #"""
-            #)
+            self.conn.execute("""
+                CREATE TABLE IF NOT EXISTS tasks (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    description TEXT,
+                    column_id INTEGER,
+                    FOREIGN KEY(column_id) REFERENCES columns(id)
+                )
+            """
+            )
 
     def get_data(self):
         cursor = self.conn.cursor()
         cursor.execute("SELECT * FROM columns")
-        data = cursor.fetchall()
-        return [Column(id=row[0], name=row[1]) for row in data]
+        columns = cursor.fetchall()
+        cursor.execute("SELECT * FROM tasks")
+        task = cursor.fetchall()
+        columns = [
+            Column(
+                id=row[0], 
+                name=row[1], 
+                tasks = [
+                    Task(
+                        id=taskRow[0], 
+                        title=taskRow[1], 
+                        description=taskRow[2], 
+                        column_id=taskRow[3])
+                        for taskRow in task if taskRow[3] == row[0]
+                ]
+                   ) for row in columns]
+        return columns
 
-    def add_column(self, name):
+    def add_column(self, name: str):
         with self.conn:
             self.conn.execute("INSERT INTO columns (name) VALUES (?)", (name,))
 
     
-    def delete_column(self, column_id):
+    def delete_column(self, column_id: int):
         with self.conn:
             self.conn.execute("DELETE FROM columns WHERE id = ?", (column_id,))
 
-    #def add_task(self, title, description, column_id):
-    #    with self.conn:
-    #        self.conn.execute(
-    #            "INSERT INTO tasks (title, description, column_id) VALUES (?, ?, ?)",
-    #            (title, description, column_id)
-    #        )
-#
-    #def get_tasks(self, column_id):
-    #    cursor = self.conn.cursor()
-    #    cursor.execute("SELECT * FROM tasks WHERE column_id = ?", (column_id,))
-    #    return cursor.fetchall()
-#
-    #def move_task(self, task_id, new_column_id):
-    #    with self.conn:
-    #        self.conn.execute(
-    #            "UPDATE tasks SET column_id = ? WHERE id = ?",
-    #            (new_column_id, task_id)
-    #        )
-#
-    #def delete_task(self, task_id):
-    #    with self.conn:
-    #        self.conn.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+    def add_task(self, title:str, description:str, column_id:int):
+        with self.conn:
+            self.conn.execute(
+                "INSERT INTO tasks (title, description, column_id) VALUES (?, ?, ?)",
+                (title, description, column_id)
+            )
+
+    def get_tasks(self, column_id):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM tasks WHERE column_id = ?", (column_id,))
+        return cursor.fetchall()
+    
+    def move_task(self, task_id, new_column_id):
+        with self.conn:
+            self.conn.execute(
+                "UPDATE tasks SET column_id = ? WHERE id = ?",
+                (new_column_id, task_id)
+            )
+
+    def delete_task(self, task_id):
+        with self.conn:
+            self.conn.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
