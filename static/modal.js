12fc71be055e57ad5a2ba2a35f5c2998720b2a6a
balintkeri <balintkeri0515@gmail.com>
2025-09-12 11:41:44 +0200

    Add Task presenter

--- static/modal.js
+++ static/modal.js
@@ -1,17 +1,36 @@
 // Modal logic
 var modal = document.getElementById("addColumnModal");
 var btn = document.getElementById("addColumnBtn");
-var span = document.getElementsByClassName("close")[0];
+var columnSpan = document.getElementById("closeColumnModal");
+
+var taskModal = document.getElementById("addTaskModal");
+var taskSpan = document.getElementById("closeTaskModal");
 
 btn.onclick = function() {
     modal.style.display = "block";
     document.getElementById("columnName").focus();
 }
-span.onclick = function() {
+columnSpan.onclick = function() {
     modal.style.display = "none";
+    document.getElementById("columnName").value = "";
 }
 window.onclick = function(event) {
 if (event.target == modal) {
     modal.style.display = "none";
 }
+}
+
+
+taskSpan.onclick = function() {
+    taskModal.style.display = "none";
+    document.getElementById("taskTitle").value = "";
+    document.getElementById("taskDescription").value = "";
+    document.getElementById("taskColumnId").value = "";
+}
+
+
+function addTask(columnId) {
+    taskModal.style.display = "block";
+    document.getElementById("taskTitle").focus();
+    document.getElementById("taskColumnId").value = columnId;
 }
